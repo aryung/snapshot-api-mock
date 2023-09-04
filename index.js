a927fad35fc75bd530fa284c79d82b1660a84c89
AC <aryung@gmail.com>
2023-09-04 11:42:56 +0800

    fix: change dataset

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const ledOuts2 = { r: [], g: [], b: [] };
 const recordDate = Date.now();
 // const { dataset } = require("./dataset");
 import { dataset } from "./dataset.js";
-let data = JSON.stringify({
-  query: `mutation PostControllerInitiative(
+let data = (playerId) =>
+  JSON.stringify({
+    query: `mutation PostControllerInitiative(
   $playerId: UUID!
   # $topic: ControllerInitiativeTopicEnums!
   $payload: String!
@@ -23,13 +24,14 @@ let data = JSON.stringify({
     message
   }
 }`,
-  variables: {
-    playerId: "62386139-6236-6366-3833-626600000000",
-    payload: JSON.stringify(dataset(Date.now())),
-  },
-});
+    variables: {
+      // playerId: "62386139-6236-6366-3833-626600000000",
+      playerId,
+      payload: JSON.stringify(dataset(Date.now())),
+    },
+  });
 
-let config = {
+let config3 = {
   method: "post",
   maxBodyLength: Infinity,
   url: "http://localhost:2000/services/graphql",
@@ -38,11 +40,32 @@ let config = {
     source: "screenhub-agent",
     "Content-Type": "application/json",
   },
-  data: data,
+  data: data("62386139-6236-6366-3833-626600000003"),
+};
+
+let config5 = {
+  method: "post",
+  maxBodyLength: Infinity,
+  url: "http://localhost:2000/services/graphql",
+  headers: {
+    Authorization: "",
+    source: "screenhub-agent",
+    "Content-Type": "application/json",
+  },
+  data: data("62386139-6236-6366-3833-626600000005"),
 };
 
 axios
-  .request(config)
+  .request(config3)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+axios
+  .request(config5)
   .then((response) => {
     console.log(JSON.stringify(response.data));
   })
